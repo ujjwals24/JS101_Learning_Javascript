cadcac08e7784c747689797b130af1fb15ee5aee
ujjwals24 <ujjwalrajput645@gmail.com>
2022-10-12 18:17:06 +0530

    introduction to web 23

---
+++ problem2.js
@@ -0,0 +1,6 @@
+let name="ujjwal";
+console.log(name);
+let father_name="mr narender ";
+console.log(father_name);
+let mother_name="ms geeta";
+console.log(mother_name)
---
+++ problem3.js
@@ -0,0 +1,6 @@
+let name="ujjwal";
+let age=19;
+
+console.log(name,age);
+console.log(typeof(name));
+console.log(typeof(age));
